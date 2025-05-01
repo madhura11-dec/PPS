a12aea1b1e418110af4f66570c7d3efa30aed6fa
madhura11-dec <ganekarmadhura@gmail.com>
2025-05-01 23:06:53 +0530

    Add files via upload

---
+++ two numbers are input to keyboard into location A & B. Write a program ti interchange conrtained of A & B using an extra variable c..c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+	int A, B, C;
+	printf ("Enter A");
+	scanf ("%d", & A);
+	printf ("Enter B");
+	scanf ("%d", & B);
+	
+	A = A + B;
+	B + A - B;
+	C = A - B;
+	
+	printf ("Initial value of A is %d", A);
+	printf ("Initial value of B is %d", B);
+	return 0;
+}
